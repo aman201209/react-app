1a521f00a40e35997f78bf9c26bea427a6b8ee8a
aman201209 <mramankushwah30@gmail.com>
2024-10-24 11:24:07 +0530

    update

--- src/Component/Booking.jsx
+++ src/Component/Booking.jsx
@@ -1,80 +1,95 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import TimePicker from 'react-time-picker'
+import 'react-time-picker/dist/TimePicker.css'
+import 'react-clock/dist/Clock.css'
+
+
+
 
 function Booking() {
-  const [data, setData] = useState({ trainName: '', trainCode: '' });
-  const [station, setStation] = useState({ stationName: '', stationCode: '' });
-  const [root, setRoot] = useState([]);
-  const [count, setCount] = useState(0);
-  const [trainData, setTrainData] = useState(null);
+  const [data, setData] = useState({ trainName: '', trainCode: '' })
+  const [station, setStation] = useState({ stationName: '', stationCode: '' })
+  const [station1, setStation1] = useState([])
+  const [root, setRoot] = useState([])
+  const [count, setCount] = useState(0)
   const [rootTrain, setRootTrain] = useState({
     id:"",
-    trainDay: {
-      monday: '',
-      tuesday: '',
-      wednesday: '',
-      thursday: '',
-      friday: '',
-      saturday: '',
-      sunday: ''
-    },
+    trainDay:[ {
+      monday: false,
+      tuesday: false,
+      wednesday: false,
+      thursday: false,
+      friday: false,
+      saturday: false,
+      sunday: false
+    }],
     trainRoot: [
-      { name: '', arri: '', dipa: '' }
+      { name: '', arri: '00:00', dipa: '00:00' }
     ]
-  });
+  })
 
+  
+console.log(rootTrain)
   const handleClick = async () => {
     try {
-      let res = await axios.post('http://localhost:3000/trainRoot', data);
-      console.log(res);
-      setRoot([...root, data]);
+      let res = await axios.post('http://localhost:3000/trainRoot', data)
+      console.log(res)
+      setRoot([...root, data])
       window.location.reload()
-      localStorage.setItem('trainCode', data.trainCode);
+      localStorage.setItem('trainCode', data.trainCode)
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
   const handleStation = async () => {
     try {
-      let res = await axios.post('http://localhost:3000/stationCode', station);
-      console.log(res);
+      let res = await axios.post('http://localhost:3000/stationCode', station)
+      console.log(res)
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   useEffect(() => {
     const user = async () => {
-      let res = await axios.post('http://localhost:3000/getStation');
-      setRoot(res.data.trainRoot);
-      console.log(res)
-        };
-    user();
-  }, []);
+      let res = await axios.post('http://localhost:3000/getStation')
+      setRoot(res.data.trainRoot)
+      setStation1(res.data.stationData)
+        }
+    user()
+  }, [])
 
   const handleCount = () => {
-    setCount(count + 1);
+    setCount(count + 1)
     setRootTrain({
       ...rootTrain,
       trainRoot: [...rootTrain.trainRoot, { name: '', arri: '', dipa: '' }]
-    });
-  };
+    })
+  }
 
   const handleTrainRoot = (index, field, value) => {
-    const newTrainRoot = [...rootTrain.trainRoot];
-    newTrainRoot[index][field] = value;
-    setRootTrain({ ...rootTrain, trainRoot: newTrainRoot });
-  };
+    const newTrainRoot = [...rootTrain.trainRoot]
+    newTrainRoot[index][field] = value
+    setRootTrain({ ...rootTrain, trainRoot: newTrainRoot })
+}
 
-  const handleTrainDay = (day, value) => {
-    setRootTrain({ ...rootTrain, trainDay: { ...rootTrain.trainDay, [day]: value } });
-  };
+  const handleTrainDay = (day, isChecked) => {
+    setRootTrain(prevState => ({
+      ...prevState,
+      trainDay: {
+        ...prevState.trainDay,
+        [day]: isChecked
+      }
+    }))
+  }
+  
   const handleTrainRoute = async()=>{
     let id = rootTrain.id
     let res = await axios.put(`http://localhost:3000/root/${id}`,rootTrain)
     console.log(res)
   }
-  console.log(rootTrain)
+  console.log(station1)
   return (
     <div>
       <div>
@@ -117,7 +132,7 @@ function Booking() {
       <div style={{ marginTop: '100px' }}>
         <div>
           <select
-            name="trainName"
+            name="id"
             value={rootTrain.id}
             onChange={e => setRootTrain({ ...rootTrain, id: e.target.value })}
           >
@@ -127,46 +142,61 @@ function Booking() {
               </option>
             ))}
           </select>
-          <input type="date" name='trainDate' value={rootTrain.trainDay} onChange={(e)=>setRootTrain({...rootTrain,[e.target.name]:e.target.value})} />
+        </div>
+        <div>
+        {['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'].map(
+  day => (
+    <React.Fragment key={day}>
+      <label>{day}</label>
+      <input
+        type="checkbox"
+        checked={rootTrain.trainDay[day]}
+        name={day}
+        onChange={e => handleTrainDay(day, e.target.checked)}
+      />
+    </React.Fragment>
+  )
+)}
+
         </div>
         <div>
           {rootTrain.trainRoot.map((station, index) => (
             <div key={index}>
-              <input
-                type="text"
-                placeholder="Station Name"
-                value={station.name}
-                onChange={e => handleTrainRoot(index, 'name', e.target.value)}
-              />
-              <input
-                type="text"
+              <select
+            name="name"
+            value={station.name}
+            onChange={e => handleTrainRoot(index, 'name', e.target.value)}
+          >
+              {station1.map(data => (
+              <option key={data.stationName} value={data.stationName}>
+                {data.stationName}
+              </option>
+            ))}
+          </select>
+              {/* <input
+                type="time"
                 placeholder="Arrival Time"
                 value={station.arri}
                 onChange={e => handleTrainRoot(index, 'arri', e.target.value)}
-              />
-              <input
-                type="text"
+              /> */}
+              {/* <TimePicker onChange={""}  /> */}
+              {/* <input
+                type="time"
                 placeholder="Departure Time"
                 value={station.dipa}
                 onChange={e => handleTrainRoot(index, 'dipa', e.target.value)}
-              />
+              /> */}
+              <TimePicker
+                  value={rootTrain.trainRoot[index].arri}
+                  onChange={(time) => handleTrainRoot(index, 'arri', time)}
+                />
+              <TimePicker
+                  value={rootTrain.trainRoot[index].dipa}
+                  onChange={(time) => handleTrainRoot(index, 'dipa', time)}
+                />
               {index === count && <button onClick={handleCount}>+</button>}
             </div>
           ))}
-        </div>
-        <div>
-          {['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'].map(
-            day => (
-              <input
-                key={day}
-                type="boolean"
-                placeholder={day}
-                value={rootTrain.trainDay[day]}
-                name={day}
-                onChange={e => handleTrainDay(day, e.target.value)}
-              />
-            )
-          )}
         </div>
       </div>
       <div>
@@ -176,7 +206,7 @@ function Booking() {
       </div>
       
     </div>
-  );
+  )
 }
 
-export default Booking;
+export default Booking
--- src/Component/BookingTicket.jsx
+++ src/Component/BookingTicket.jsx
@@ -1,43 +1,154 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import {Link} from "react-router-dom"
+import moment from 'moment'
 
 function BookingTicket() {
     let [user , setUser] = useState({
         date:"",
     })
-    let [train , setTrain] = useState([])
-    console.log(user)
-
+    let [day , setDay] = useState(false)
+    let [train , setTrain] = useState({
+        id:""
+    })
+    let [stations,setStations] = useState([])
+    let [root , setRoot] = useState([])
+    let [route , setroute] = useState([])
+    let [station , setStation] = useState({
+        from:'',
+        to:"",
+    })
+    let data0 = moment(user.date).format("dddd")
     useEffect(()=>{
         let data = async()=>{
             let res =await axios.post("http://localhost:3000/getStation")
-            let date = res.data.trainRoot.filter(value=>value.trainDate==user.date)
-            console.log(date)
-            setTrain(date)
-        console.log(res.data.trainRoot)
+            setRoot(res.data.trainRoot)
+            setStations(res.data.stationData)
         }
         data()
     },[])
     const handleClick = ()=>{
-        
+        setDay(true)
+        let data = root.find(value=>value._id === train.id)
+        setroute(data)
+        console.log(data)
+    }
+    const handleHtml =(id)=>{
+        if(id == true){
+            return("Yes")
+        }else{
+            return("No")
+        }
+    }
+    const handle =()=>{
+            console.log(data0,station)
+
     }
   return (
     <div>
-        <div>
+            <div>
+                Date
             <input type="date" name='date' value={user.date} onChange={(e)=>setUser({...user,[e.target.name]:e.target.value})} />
-            <button onClick={{handleClick}}>
+            </div>
+        <div>
+        <select
+            name="from"
+            value={station.from}
+            onChange={e => setStation({ ...station, [e.target.name]: e.target.value })}
+          >
+            {stations.map(data => (
+              <option key={data.stationName} value={data.stationName}>
+                {data.stationName}
+              </option>
+            ))}
+          </select>
+        <select
+            name="to"
+            value={station.to}
+            onChange={e => setStation({ ...station, [e.target.name]: e.target.value })}
+          >
+            {stations.map(data => (
+              <option key={data.stationName} value={data.stationName}>
+                {data.stationName}
+              </option>
+            ))}
+          </select>
+            <button onClick={handle}>
+                check
+            </button>
+        </div>
+        <div>
+            <select
+            name="id"
+            value={train.id}
+            onChange={e => setTrain({ ...train, id: e.target.value })}
+          >
+            {root.map(data => (
+              <option key={data._id} value={data._id}>
+                {data.trainName}
+              </option>
+            ))}
+          </select>
+            <button onClick={handleClick}>
                 Check
             </button>
         </div>
         <div>
-            <table>
-                <tr>
-                    <th>Train Name</th>
-                    <th>Train Date</th>
-                    <th>Train Date</th>
-                </tr>
-            </table>
+            <div>Train Name:- {route.trainName}</div>
+            
+                {
+                    day && <div>
+                        <table style={{border:"1px solid black", display:"flex",alignItems:"center",justifyContent:"center",flexDirection:"column",width:"100%"}}>
+                            <tr style={{width:"100%",display:"flex",alignItems:"center",justifyContent:"center",flexDirection:"row",}}>
+                                <th style={{width:"300px"}}>Station Name</th>
+                                <th style={{width:"300px"}}>Arrival Time</th>
+                                <th style={{width:"300px"}}>Departure Time</th>
+                            </tr>
+                        {
+                        route.trainRoot.map((value)=>(
+                            <tr style={{width:"100%",display:"flex",alignItems:"center",justifyContent:"center",flexDirection:"row",}}>
+                                <td  style={{width:"300px",display:"flex",alignItems:"center",justifyContent:"center"}}>{value.name}</td>
+                                <td style={{width:"300px",display:"flex",alignItems:"center",justifyContent:"center"}}>{value.arri}</td>
+                                <td style={{width:"300px",display:"flex",alignItems:"center",justifyContent:"center"}}>{value.dipa}</td>
+                            </tr>
+                        ))
+                    }
+                    </table>
+                    </div>
+                }
+                {
+                    day && <div>
+                        <table style={{border:"1px solid black", display:"flex",alignItems:"center",justifyContent:"center",flexDirection:"column",width:"100%"}}>
+                            <tr style={{width:"100%",display:"flex",alignItems:"center",justifyContent:"center",flexDirection:"row",gap:"60px"}}>
+                                <th style={{width:"300px",display:"flex",alignItems:"center",justifyContent:"center"}}>Monday</th>
+                                <th style={{width:"300px",display:"flex",alignItems:"center",justifyContent:"center"}}>Tuesday</th>
+                                <th style={{width:"300px",display:"flex",alignItems:"center",justifyContent:"center"}}>Wednesday</th>
+                                <th style={{width:"300px",display:"flex",alignItems:"center",justifyContent:"center"}}>Thursday</th>
+                                <th style={{width:"300px",display:"flex",alignItems:"center",justifyContent:"center"}}>Friday</th>
+                                <th style={{width:"300px",display:"flex",alignItems:"center",justifyContent:"center"}}>Saturday</th>
+                                <th style={{width:"300px",display:"flex",alignItems:"center",justifyContent:"center"}}>Sunday</th>
+                            </tr>
+                            <tr style={{width:"100%",display:"flex",alignItems:"center",justifyContent:"center",flexDirection:"row",gap:"60px"}}>
+                                <td style={{width:"300px",display:"flex",alignItems:"center",justifyContent:"center"}}>{handleHtml(route.trainDay[0].monday)}</td>
+                                <td style={{width:"300px",display:"flex",alignItems:"center",justifyContent:"center"}}>{handleHtml(route.trainDay[0].tuesday)}</td>
+                                <td style={{width:"300px",display:"flex",alignItems:"center",justifyContent:"center"}}>{handleHtml(route.trainDay[0].wednesday)}</td>
+                                <td style={{width:"300px",display:"flex",alignItems:"center",justifyContent:"center"}}>{handleHtml(route.trainDay[0].thursday)}</td>
+                                <td style={{width:"300px",display:"flex",alignItems:"center",justifyContent:"center"}}>{handleHtml(route.trainDay[0].firday)}</td>
+                                <td style={{width:"300px",display:"flex",alignItems:"center",justifyContent:"center"}}>{handleHtml(route.trainDay[0].saturday)}</td>
+                                <td style={{width:"300px",display:"flex",alignItems:"center",justifyContent:"center"}}>{handleHtml(route.trainDay[0].sunday)}</td>
+                            </tr>
+                        </table>
+                                {/* <div>Monday:-{handleHtml(route.trainDay[0].monday)}</div>
+                                <div>Tuesday:-{handleHtml(route.trainDay[0].tuesday)}</div>
+                                <div>Wednesday:-{handleHtml(route.trainDay[0].wednesday)}</div>
+                                <div>Thursday:-{handleHtml(route.trainDay[0].thursday)}</div>
+                                <div>Friday:-{handleHtml(route.trainDay[0].firday)}</div>
+                                <div>Saturday:-{handleHtml(route.trainDay[0].saturday)}</div>
+                                <div>Sunday:-{handleHtml(route.trainDay[0].sunday)}</div> */}
+                            
+                            </div>
+                }
+            
         </div>
         <Link to={"/booking"}>hello</Link>
     </div>
@@ -45,3 +156,4 @@ function BookingTicket() {
 }
 
 export default BookingTicket
+
